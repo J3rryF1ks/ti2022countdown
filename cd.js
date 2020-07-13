44498241c54c8b26b274694c388f0fa4b618493c
MrHome1ander <40196875+MrHome1ander@users.noreply.github.com>
2020-07-13 22:57:07 +0200

    Add files via upload

---
+++ cd.js
@@ -0,0 +1,38 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const ti2022 = new Date(`August 15 2022 00:00:00`);
+
+// Set background year
+year.innerText = 2022;
+
+// Update countdown time
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = ti2022 - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    // Add values to DOM
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+// Show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000);
+
+// Run every second
+setInterval(updateCountdown, 1000);
